2e6924f718da825a40d2297c60f71bdd489fd4de
Raphaël PICARD <raphaelpicard@ymail.com>
2022-05-28 16:51:42 +0200

    feat: Modification de la page Home

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,30 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.scss";
+
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Raphaël PICARD</title>
+        <meta charSet="utf-8" />
+        <meta
+          name="description"
+          content="Site internet du développeur informatique Raphaël PICARD"
+        />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="keywords"
+          content="reactjs, nextjs, unity3d, développeur front end, développeur back end, développeur full stack, développeur informatique, developpeur, developpeur web, freelance, développeur freelance"
+        />
+        <meta name="author" content="Raphaël PICARD" />
+        <meta name="copyright" content="Raphaël PICARD" />
+        <meta name="robots" content="index" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
