bb0b27e3aff56b495e4534e098c7ec332bc22071
Raphaël PICARD <raphaelpicard@ymail.com>
2022-05-28 16:59:45 +0200

    fix: Correction orthographe

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,13 +8,13 @@ const Home: NextPage = () => {
       <p className={styles["start"]}>Site en construction</p>
       <div id={styles["titles"]}>
         <div id={styles["titlecontent"]}>
-          <p>Bienvenu sur mon site web perso !</p>
+          <p>Bienvenue sur mon site web !</p>
           <p>
             Je m&apos;appelle <strong>Raphaël PICARD</strong>.
           </p>
           <p>Je suis Développeur Web/Mobile depuis maintenant 6ans.</p>
-          <p>Actuellement mon site est en cours de construction !</p>
-          <p>Vous pouvez cependant trouver ci-joint mon CV et mon linkedin.</p>
+          <p>Actuellement mon site est en cours de refonte !</p>
+          <p>Vous pouvez cependant avoir accès à mon CV et mon linkedin.</p>
           <p style={{ textAlign: "center" }}>
             <a
               href="https://www.linkedin.com/in/raphael-picard/"
